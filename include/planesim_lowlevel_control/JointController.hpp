5bdcc22f03654c40379dc7d91caa1a92b71886c7
Bill Fan <1472451850@qq.com>
2020-04-30 13:06:40 -0400

    Added PID control

--- include/planesim_lowlevel_control/JointController.hpp
+++ include/planesim_lowlevel_control/JointController.hpp
@@ -2,23 +2,45 @@
 #define JOINTCONTROLLER_HPP
 
 #include "ros/ros.h"
+#include "sensor_msgs/JointState.h"
 
 #include <thread>
+#include <chrono>
+#include <array>
+#include <string>
+#include <map>
+
+#include "../include/planesim_lowlevel_control/PIDFF.hpp"
 
 class JointController {
 public:
     JointController(ros::NodeHandle nh);
     ~JointController();
 
-    void start(unsigned int updateIntervalMS);
-    void stop();
+    void init();
+
+    void onJointState(const sensor_msgs::JointStateConstPtr &jointState);
 
 private:
     ros::NodeHandle n;
+    
+    const std::map<std::string, int> jointsMap = {
+        {"propeller_joint", 0},
+        {"left_aileron_joint", 1},
+        {"right_aileron_joint", 2},
+    };
 
-    std::atomic<bool> running{false};
-    std::thread mainThread;
+    std::array<ros::Time, 3> lastUpdateTimes;
+    std::array<float, 3> Targets;
+    std::array<PIDFF, 3> PIDs;
+    std::array<float, 3> CMDs;
 
+    ros::Time propellerLastUpdateTime;
+    float propellerTarget;
+    PIDFF propellerPID;
+    float propellerCMD;
+
+    ros::Subscriber jointStateSub;
 };
 
 #endif
--- src/JointController.cpp
+++ src/JointController.cpp
@@ -2,11 +2,36 @@
 
 JointController::JointController(ros::NodeHandle nh){
     this->n = nh;
+    for (ros::Time time : lastUpdateTimes){
+        time = ros::Time::now();
+    }
+    propellerLastUpdateTime = ros::Time::now();
+
+    jointStateSub = n.subscribe("/planesim/joint_state", 1000, &JointController::onJointState, this);
 }
 
 JointController::~JointController(){
 }
 
-void JointController::start(unsigned int updateIntervalMS){
-    
+void JointController::init(){
+    ROS_INFO("Spinning node!");
+    ros::spinOnce;
+}
+
+void JointController::onJointState(const sensor_msgs::JointState::ConstPtr &jointStatePtr){
+
+    int counter = 0;
+
+    std::vector<double> position = jointStatePtr->position;
+    std::vector<double> velocity = jointStatePtr->velocity;
+
+    for (std::string name : jointStatePtr->name){
+        int index = jointsMap.find(name)->second;
+        float dt = ros::Time::now().toSec - lastUpdateTimes[index].toSec;
+
+        CMDs[index] = PIDs[index].update(Targets[index], index == 0 ? velocity[counter] : position[counter], dt);
+
+        counter++;
+        lastUpdateTimes[index] = ros::Time::now();
+    }
 }
